aa6a4089fed20f70248a4b74aa33b703f5a9ddbb
etom <etom@DESKTOP-ANLVEMJ>
2022-04-06 15:32:18 +0800

    Add project files.

---
+++ Web FoodBasket/Pages/CreateAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_FoodBasket.Pages
+{
+    public class CreateAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Web FoodBasket/Pages/LogIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_FoodBasket.Pages
+{
+    public class LogInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
